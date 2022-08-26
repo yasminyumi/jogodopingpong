66e06950ca5aab4dc8782ac82762d8ee1f1f95a9
Yasmin Yumi <106815328+yasminyumi@users.noreply.github.com>
2022-08-26 10:47:35 -0300

    Add files via upload

---
+++ trabalho_pingpong/U_pong.pas
@@ -0,0 +1,88 @@
+unit U_pong;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    bola: TPanel;
+    gol2: TPanel;
+    moverdireita: TTimer;
+    gol1: TPanel;
+    moveresquerda: TTimer;
+    procedure moverdireitaTimer(Sender: TObject);
+    procedure moveresquerdaTimer(Sender: TObject);
+
+    procedure FormKeyDown(Sender: TObject; var Key: Word;
+      Shift: TShiftState);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure colisaogol2;
+var
+corpo:integer;
+
+begin
+corpo:= form1.bola.left + form1.bola.Width;
+if corpo >= form1.gol2.left then
+begin
+form1.moverdireita.enabled := false;
+form1.moveresquerda.enabled := true;
+end;
+ end;
+
+procedure colisaogol1;
+var
+corpo1:integer;
+begin
+corpo1:= form1.bola.left + form1.bola.Width;
+if corpo1 >= form1.gol1.left then
+begin
+form1.moverdireita.enabled := true;
+form1.moveresquerda.enabled := false;
+end;
+end;
+
+procedure TForm1.moverdireitaTimer(Sender: TObject);
+begin
+ bola.left := bola.left + 5;
+ colisaogol2;
+end;
+
+
+procedure TForm1.moveresquerdaTimer(Sender: TObject);
+begin
+bola.Left := bola.Left - 5;
+
+end;
+
+procedure TForm1.FormKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+if key = 40 then
+begin
+ gol2.top := gol2.top + 10;
+ gol1.top := gol1.top + 10;
+end;
+if key = 38 then
+gol2.top := gol2.top - 10;
+gol1.top := gol1.top - 10;
+
+end;
+
+end.
+
+
---
+++ trabalho_pingpong/U_pong.~dfm
@@ -0,0 +1,54 @@
+object Form1: TForm1
+  Left = 241
+  Top = 214
+  Width = 1305
+  Height = 675
+  Caption = 'Form1'
+  Color = clBlack
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  OnKeyDown = FormKeyDown
+  PixelsPerInch = 96
+  TextHeight = 13
+  object bola: TPanel
+    Left = 656
+    Top = 272
+    Width = 33
+    Height = 33
+    Caption = 'bola'
+    TabOrder = 0
+  end
+  object gol2: TPanel
+    Left = 1185
+    Top = 232
+    Width = 63
+    Height = 201
+    Caption = 'gol2'
+    TabOrder = 1
+  end
+  object gol1: TPanel
+    Left = 25
+    Top = 200
+    Width = 63
+    Height = 201
+    Caption = 'gol1'
+    TabOrder = 2
+  end
+  object moverdireita: TTimer
+    Interval = 20
+    OnTimer = moverdireitaTimer
+    Left = 152
+    Top = 64
+  end
+  object moveresquerda: TTimer
+    Enabled = False
+    Interval = 20
+    OnTimer = moveresquerdaTimer
+    Left = 192
+    Top = 64
+  end
+end
